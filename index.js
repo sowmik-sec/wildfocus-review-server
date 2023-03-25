755ddfdf369a8a0d0e29c570597484b144e41023
sowmik-sec <sowmik.sec@gmail.com>
2023-03-25 15:43:06 +0600

    initializing mongodb server and creating services api

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const services = require("./services.json");
+require("dotenv").config();
 
 // middleware
 app.use(cors());
@@ -12,9 +14,36 @@ app.get("/", (req, res) => {
   res.send("Hello World of expressjs");
 });
 
-app.get("/services", (req, res) => {
-  res.send(services);
+// pass: f0nGTcho5GzNpIyU
+// user: wildfocusreview
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xgh8h2c.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
+
+const run = async () => {
+  try {
+    const serviceCollection = client
+      .db("wildFocusReview")
+      .collection("services");
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+};
+
+run().catch((err) => console.error(err));
+
+// app.get("/services", (req, res) => {
+//   res.send(services);
+// });
 app.get("/services-3", (req, res) => {
   res.send(services.slice(0, 3));
 });

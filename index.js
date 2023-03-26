cab820b2185822d8774b1cba14963bf417d40a05
sowmik-sec <sowmik.sec@gmail.com>
2023-03-26 15:04:59 +0600

    review deleted from server

--- index.js
+++ index.js
@@ -72,6 +72,12 @@ const run = async () => {
       const result = await reviewCollection.insertOne(review);
       res.send(result);
     });
+    app.delete("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 };
